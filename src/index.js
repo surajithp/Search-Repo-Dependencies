65d6962ab807bb0909c4b65a495430fb845de1b4
surajith <surajith1991@gmail.com>
2018-12-12 20:49:11 +0530

    Modified Readme file and index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class GithubRepoSearch extends React.Component{
+    render(){
+        return(
+            <div>
+                Hi There
+            </div>
+        )
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<GithubRepoSearch/>,document.getElementById("root"))

0dbcd53629b797dbe1932f2b592b39dbe597ddf2
surajith <surajith1991@gmail.com>
2018-12-12 21:17:20 +0530

    Added state to SearchBar component to store search keyword which we enter

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { searchkeyword: "" };
+
+  OnFormsubmit = event => {
+    event.preventDefault();
+  };
   render() {
     return (
       <div>
-        <form>
-          <input type="text" placeholder="Search here" />
+        <form onSubmit={this.OnFormsubmit}>
+          <input
+            type="text"
+            placeholder="Search here"
+            onChange={event =>
+              this.setState({ searchkeyword: event.target.value })
+            }
+          />
+          <button type="submit">Search</button>
         </form>
-        <button type="submit">Search</button>
       </div>
     );
   }

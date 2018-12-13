eb7dbebcdbb179fde1299c21f8aa52cd31fa268a
surajith <surajith1991@gmail.com>
2018-12-13 10:52:12 +0530

    Added state in App to pass search keyword to Pagination for buttons click

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,54 @@ import DisplayRepos from "./DisplayRepos";
 import Pagination from "./Pagination";
 
 class App extends React.Component {
-  state = { data: [], pagenumber: 0 };
+  state = { data: [], pagenumber: 0, searchkeyword: "" };
   onReposearch = async keyword => {
     const response = await axios.get(
       `https://api.github.com/search/repositories?q=${keyword}+language:javascript&sort=stars&order=desc&per_page=20&page=${this
         .state.pagenumber + 1}`
     );
+    const repo = keyword;
     let reposdata = response.data.items;
-    this.setState({ data: reposdata, pagenumber: this.state.pagenumber + 1 });
+    this.setState({
+      data: reposdata,
+      pagenumber: this.state.pagenumber + 1,
+      searchkeyword: repo
+    });
+  };
+  onPrevPageSearch = async keyword => {
+    const response = await axios.get(
+      `https://api.github.com/search/repositories?q=${keyword}+language:javascript&sort=stars&order=desc&per_page=20&page=${this
+        .state.pagenumber - 1}`
+    );
+    let reposdata = response.data.items;
+    this.setState({
+      data: reposdata,
+      pagenumber: this.state.pagenumber - 1
+    });
+  };
+  pagnation = () => {
+    if (this.state.pagenumber === 0) {
+      return null;
+    } else if (this.state.pagenumber === 1) {
+      return (
+        <Pagination
+          pagenumber={this.state.pagenumber}
+          reponame={this.state.searchkeyword}
+          clicknext={this.onReposearch}
+          disabled={true}
+        />
+      );
+    } else {
+      return (
+        <Pagination
+          pagenumber={this.state.pagenumber}
+          reponame={this.state.searchkeyword}
+          clicknext={this.onReposearch}
+          clickprev={this.onPrevPageSearch}
+          disabled={false}
+        />
+      );
+    }
   };
   render() {
     console.log(this.state.data);
@@ -20,7 +60,7 @@ class App extends React.Component {
       <div>
         <SearchBar onclick={this.onReposearch} />
         <DisplayRepos repositems={this.state.data} />
-        <Pagination pagenumber={this.state.pagenumber} />
+        <div>{this.pagnation()}</div>
       </div>
     );
   }
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-const Pagination = () => {
+const Pagination = props => {
+  const nextPageclick = () => {
+    props.clicknext(props.reponame);
+  };
+  const prevPageclick = () => {
+    props.clickprev(props.reponame);
+  };
+
   return (
-    <div>
-      <button>Prev</button>
-      Pagenumber
-      <button>Next</button>
+    <div style={{ textAlign: "center", marginBottom: "20px" }}>
+      <button disabled={props.disabled} onClick={prevPageclick}>
+        Prev
+      </button>
+      {props.pagenumber}
+      <button onClick={nextPageclick}>Next</button>
     </div>
   );
 };
